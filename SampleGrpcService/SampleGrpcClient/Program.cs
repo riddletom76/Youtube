242655948ae856d31ad7b91025d627d7e76c8f42
rishabh sharma <riddletom76@gmail.com>
2021-03-28 16:45:05 +0530

    Initial commit with the basic Project

---
+++ SampleGrpcService/SampleGrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleGrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string serviceEndpoint = "https://localhost:5001";
+            var channel = GrpcChannel.ForAddress(serviceEndpoint);
+            var client = new Greeter.GreeterClient(channel);
+            HelloRequest helloRequest = new HelloRequest {
+                Name = "Rishabh"
+            };
+            var response = await client.SayHelloAsync(helloRequest);
+
+            Console.WriteLine($"Response obtained from the service {response.Message}");
+        }
+    }
+}
