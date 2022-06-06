156172f4ff53e9321086e5c20015e7e3b7761633
Rishabh Sharma <riddletom76@gmail.com>
2022-06-06 19:52:40 +0530

    Adding application example for HttpLogging and HealthCheck

---
+++ HttpLogging/HttpLogging/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+app.UseHttpLogging();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapHealthChecks("/healthCheck");
+app.MapControllers();
+
+app.Run();
